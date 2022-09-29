0bef2353eaf39b862908e4c2a63635a40187f515
Şeyma Gündoğdu <symagn2@gmail.com>
2022-09-29 17:59:32 +0300

    antd sample

--- src/App.js
+++ src/App.js
@@ -8,43 +8,82 @@ import ChildSample from "./routerSample/ChildSample";
 import GuardSample from "./routerSample/GuardSample";
 import LoginPage from "./routerSample/LoginPage";
 import LocationSample from "./routerSample/LocationSample";
+import Products from "./contextSample/Products";
+import { useContext } from "react";
+import { CartContext } from "./contextSample/CartContext";
+import Cart from "./contextSample/Cart";
+import 'antd/dist/antd.css';
+import {  Layout, Menu } from 'antd';
+import Customers from "./components/Customers";
 
+
+const { Header, Content, Footer } = Layout;
 
 
 function App() {
 
+  const { cart } = useContext(CartContext)
+
+  const items = [
+    { label:<Link to='/'>Home</Link> , key: '1' }, 
+    { label:<Link to='/customers'>Customers</Link> , key: '2' }, 
+    { label:<Link to='/customers'>Products</Link> , key: '3' }, 
+
+  ];
 
   return (
     <>
-      <h1>Site Header</h1>
-      <ul style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <li><Link to='/'>Home</Link></li>
+
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            items={items}
+          />
+        </Header>
+        <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/products" element={<Products />}></Route>
+              <Route path="/cart" element={<Cart />}></Route>
+              <Route path="/about" element={<AboutPage />}></Route>
+              <Route path="/contact" element={<ContactPage />}></Route>
+              <Route path="/categories" element={<CategoryPage />}></Route>
+              <Route path="/categories/:id" element={<CategoryDetail />}></Route>
+              <Route path="/login" element={<LoginPage />}></Route>
+              <Route path="/customers" element={<Customers />}></Route>
+              <Route path="/guardsample" element={
+                <GuardSample>
+                  <ChildSample />
+                </GuardSample>
+              }>
+              </Route>
+              <Route path="/location" element={<LocationSample />}></Route>
+
+
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}> </Footer>
+
+
+
+      </Layout>
+
+        {/* <li><Link to='/'>Home</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/categories'>Category List</Link></li>
         <li><Link to='/guardsample'>Guard Sample</Link></li>
         <li><Link to='/location'>Location Sample</Link></li>
-      </ul>
+        <li><Link to='/products'>Products</Link></li>
+        <li><Link to='/cart'>Cart</Link></li> */}
 
 
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/about" element={<AboutPage />}></Route>
-        <Route path="/contact" element={<ContactPage />}></Route>
-        <Route path="/categories" element={<CategoryPage />}></Route>
-        <Route path="/categories/:id" element={<CategoryDetail />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/guardsample" element={
-          <GuardSample>
-            <ChildSample />
-          </GuardSample>
-        }>
-        </Route>
-        <Route path="/location" element={<LocationSample />}></Route>
 
-
-      </Routes>
-      <h1>Site Footer</h1>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { CartProvider } from './contextSample/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App></App>
+    <BrowserRouter>
+        <CartProvider>
+            <App></App>
+        </CartProvider>
+    </BrowserRouter>
+
 );
